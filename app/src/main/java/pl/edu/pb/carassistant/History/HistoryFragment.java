83816726b4e2d36c6d4e7cf3f571055207997ce7
nieswietymikolaj <73493587+nieswietymikolaj@users.noreply.github.com>
2020-12-30 02:25:48 +0100

    Dodano pasek nawigacji
    Dodano pasek nawigacji

---
+++ app/src/main/java/pl/edu/pb/carassistant/History/HistoryFragment.java
@@ -0,0 +1,16 @@
+package pl.edu.pb.carassistant.History;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.edu.pb.carassistant.R;
+
+public class HistoryFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_history);
+    }
+}
---
+++ app/src/main/java/pl/edu/pb/carassistant/Parking/ParkingFragment.java
@@ -0,0 +1,16 @@
+package pl.edu.pb.carassistant.Parking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.edu.pb.carassistant.R;
+
+public class ParkingFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_parking);
+    }
+}
