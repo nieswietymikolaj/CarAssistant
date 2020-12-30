114cff1dd4ecb9e94871b90e2dc7e911bf90b909
nieswietymikolaj <73493587+nieswietymikolaj@users.noreply.github.com>
2020-12-30 02:56:24 +0100

    Przemieszczanie się pomiędzy logowaniem a rejestracją
    Przemieszczanie się pomiędzy logowaniem a rejestracją

--- app/src/main/java/pl/edu/pb/carassistant/User/UserFragment.java
+++ app/src/main/java/pl/edu/pb/carassistant/User/UserFragment.java
@@ -1,16 +1,16 @@
 package pl.edu.pb.carassistant.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
 import pl.edu.pb.carassistant.R;
 
-public class UserFragment extends AppCompatActivity {
+public class UserFragment extends Fragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_user);
     }
 }
