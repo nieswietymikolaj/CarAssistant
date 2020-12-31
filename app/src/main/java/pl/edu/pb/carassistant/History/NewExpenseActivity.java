c5425dcbcd5791d8988f3ca5cd05ae6e92b0997a
nieswietymikolaj <73493587+nieswietymikolaj@users.noreply.github.com>
2020-12-31 02:02:03 +0100

    Poprawki
    Poprawki

---
+++ app/src/main/java/pl/edu/pb/carassistant/History/NewExpenseActivity.java
@@ -0,0 +1,16 @@
+package pl.edu.pb.carassistant.History;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.edu.pb.carassistant.R;
+
+public class NewExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_expense);
+    }
+}
