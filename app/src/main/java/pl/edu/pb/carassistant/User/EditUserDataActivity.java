65e929606d089e8d45c63fd92e62c64a21b85af5
nieswietymikolaj <73493587+nieswietymikolaj@users.noreply.github.com>
2021-01-02 00:02:08 +0100

    Dodano część widoku profilu oraz toolbar
    Dodano część widoku profilu oraz toolbar

---
+++ app/src/main/java/pl/edu/pb/carassistant/User/EditUserDataActivity.java
@@ -0,0 +1,16 @@
+package pl.edu.pb.carassistant.User;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.edu.pb.carassistant.R;
+
+public class EditUserDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_data);
+    }
+}
