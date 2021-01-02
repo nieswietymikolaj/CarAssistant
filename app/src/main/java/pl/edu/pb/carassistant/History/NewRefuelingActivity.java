6f1ea52be69407cfdf9609708f08f307e4848745
nieswietymikolaj <73493587+nieswietymikolaj@users.noreply.github.com>
2021-01-02 16:46:52 +0100

    Poprawiono walidację tankowania
    Poprawiono walidację tankowania

--- app/src/main/java/pl/edu/pb/carassistant/History/NewRefuelingActivity.java
+++ app/src/main/java/pl/edu/pb/carassistant/History/NewRefuelingActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -23,13 +24,20 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import pl.edu.pb.carassistant.MainActivity;
 import pl.edu.pb.carassistant.R;
 
 public class NewRefuelingActivity extends AppCompatActivity implements TextWatcher {
+
+    String DATE_PATTERN = "^(1[0-9]|0[1-9]|3[0-1]|2[1-9])/(0[1-9]|1[0-2])/[0-9]{4}$";
+    String TIME_PATTERN = "^(1[0-9]|0[1-9]|2[1-3]):([0-5][0-9])$";
+    String PRICE_LITERS_PATTERN = "^(([0-9]|[0-9]{2}|[0-9]{3}|[0-9]{4}|[0-9]{5}|[0-9]{6}).([0-9]|[0-9]{2}|0[0-9]))|([0-9]|[0-9]{2}|[0-9]{3}|[0-9]{4}|[0-9]{5}|[0-9]{6}|[0-9]{7}||[0-9]{8})$";
 
     EditText refuelingDate, refuelingTime, refuelingMileage, refuelingPriceLiter, refuelingCost, refuelingLiters;
     Button saveButton;
@@ -37,12 +45,22 @@ public class NewRefuelingActivity extends AppCompatActivity implements TextWatch
 
     FirebaseFirestore firebaseFirestore;
 
+    Calendar calendar;
+    SimpleDateFormat dateFormat, timeFormat;
+    String currentDate, currentTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_refueling);
 
         firebaseFirestore = FirebaseFirestore.getInstance();
+
+        calendar = Calendar.getInstance();
+        dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        currentDate = dateFormat.format(calendar.getTime());
+        timeFormat = new SimpleDateFormat("HH:mm");
+        currentTime = timeFormat.format(calendar.getTime());
 
         Toolbar toolbar = findViewById(R.id.new_refueling_toolbar);
         setSupportActionBar(toolbar);
@@ -56,6 +74,9 @@ public class NewRefuelingActivity extends AppCompatActivity implements TextWatch
         refuelingPriceLiter = findViewById(R.id.new_refueling_price_liter_text);
         refuelingCost = findViewById(R.id.new_refueling_cost_text);
         refuelingLiters = findViewById(R.id.new_refueling_liters_text);
+
+        refuelingDate.setText(currentDate);
+        refuelingTime.setText(currentTime);
 
         refuelingDate.addTextChangedListener(this);
         refuelingTime.addTextChangedListener(this);
@@ -75,34 +96,6 @@ public class NewRefuelingActivity extends AppCompatActivity implements TextWatch
             }
         });
     }
-
-    /*private void SaveUserInDatabase(String name, String brand, String model, String year, String mileage, String registration) {
-        String userId = firebaseAuth.getUid();
-        DocumentReference documentReference = firebaseFirestore.collection("users").document(userId);
-
-
-        documentReference.set(user).addOnSuccessListener(aVoid -> {
-            Toast.makeText(this, getResources().getString(R.string.new_user_data_added), Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }).addOnFailureListener(e -> Toast.makeText(this, getResources().getString(R.string.new_user_error) + " " + e.getLocalizedMessage(), Toast.LENGTH_LONG).show());
-    }*/
-
-
-
-    /*firebaseAuth.sendPasswordResetEmail(email).addOnSuccessListener(new OnSuccessListener<Void>() {
-        @Override
-        public void onSuccess(Void aVoid) {
-            Toast.makeText(context, activity.getResources().getString(R.string.reset_password_sent), Toast.LENGTH_LONG).show();
-            dialog.dismiss();
-        }
-    }).addOnFailureListener(new OnFailureListener() {
-        @Override
-        public void onFailure(@NonNull Exception e) {
-            Toast.makeText(context, activity.getResources().getString(R.string.new_user_error) + " " + e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-    });*/
 
     private void AddNewRefueling() {
         String date = refuelingDate.getText().toString().trim();
@@ -150,12 +143,24 @@ public class NewRefuelingActivity extends AppCompatActivity implements TextWatch
             refuelingDate.setBackgroundResource(R.drawable.edit_text_error);
             return false;
         }
+
+        if (!date.matches(DATE_PATTERN)) {
+            refuelingDate.setError(getString(R.string.new_refueling_date_error));
+            refuelingDate.setBackgroundResource(R.drawable.edit_text_error);
+            return false;
+        }
         return true;
     }
 
     private boolean ValidateTime(String time) {
         if (time.isEmpty()) {
             refuelingTime.setError(getString(R.string.new_user_error_empty));
+            refuelingTime.setBackgroundResource(R.drawable.edit_text_error);
+            return false;
+        }
+
+        if (!time.matches(TIME_PATTERN)) {
+            refuelingTime.setError(getString(R.string.new_refueling_time_error));
             refuelingTime.setBackgroundResource(R.drawable.edit_text_error);
             return false;
         }
@@ -177,6 +182,12 @@ public class NewRefuelingActivity extends AppCompatActivity implements TextWatch
             refuelingPriceLiter.setBackgroundResource(R.drawable.edit_text_error);
             return false;
         }
+
+        if (!priceLiter.matches(PRICE_LITERS_PATTERN)) {
+            refuelingPriceLiter.setError(getString(R.string.new_refueling_price_liters_error));
+            refuelingPriceLiter.setBackgroundResource(R.drawable.edit_text_error);
+            return false;
+        }
         return true;
     }
 
@@ -186,12 +197,24 @@ public class NewRefuelingActivity extends AppCompatActivity implements TextWatch
             refuelingCost.setBackgroundResource(R.drawable.edit_text_error);
             return false;
         }
+
+        if (!cost.matches(PRICE_LITERS_PATTERN)) {
+            refuelingCost.setError(getString(R.string.new_refueling_price_liters_error));
+            refuelingCost.setBackgroundResource(R.drawable.edit_text_error);
+            return false;
+        }
         return true;
     }
 
     private boolean ValidateLiters(String liters) {
         if (liters.isEmpty()) {
             refuelingLiters.setError(getString(R.string.new_user_error_empty));
+            refuelingLiters.setBackgroundResource(R.drawable.edit_text_error);
+            return false;
+        }
+
+        if (!liters.matches(PRICE_LITERS_PATTERN)) {
+            refuelingLiters.setError(getString(R.string.new_refueling_price_liters_error));
             refuelingLiters.setBackgroundResource(R.drawable.edit_text_error);
             return false;
         }
@@ -216,7 +239,13 @@ public class NewRefuelingActivity extends AppCompatActivity implements TextWatch
 
     @Override
     public void afterTextChanged(Editable s) {
+        /*float priceLiterNumber = Float.parseFloat(refuelingPriceLiter.getText().toString().trim());
+        float costNumber = Float.parseFloat(refuelingCost.getText().toString().trim());
+        float litersNumber = Float.parseFloat(refuelingLiters.getText().toString().trim());
 
+        double calculatedValue  = ( (double ) costNumber/priceLiterNumber);
+
+        refuelingLiters.setText(Double.toString(calculatedValue));*/
     }
 
     @Override
